fa027fd415a096ef05e87401ba01ed2c3fa25303
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-06 22:34:44 +1000

    Day 6 part 2 - another attempt, hopefully works, should optimise at some point

--- 06/sol_part2_attempt2.py
+++ 06/sol_part2_attempt2.py
@@ -1,6 +1,8 @@
 # itoombes, Advent of Code
 # Day 6
 
+import copy
+
 def read_maze():
     f = open("input.txt", "r")
     lines = list()
@@ -18,17 +20,74 @@ def read_maze():
     return lines, col, j
 
 def main():
-    maze, guardRow, guardCol = read_maze()
+    ogMaze, guardRow, guardCol = read_maze()
+    nRows = len(ogMaze)
+    nCols = len(ogMaze[0])
+    
+    count = 0
+
     # Iterate thru all possible squares
-    for i in range(0, len(maze)):
-        for j in range(0, len(maze[0])):
+    for i in range(0, nRows):
+        for j in range(0, nCols):
             print(f"({i}, {j})")
-            if maze[i][j] == "^":
+            if ogMaze[i][j] == "^":
                 print("\tGuard location - skip")
                 continue
-            if maze[i][j] == "#":
+            if ogMaze[i][j] == "#":
                 print("\tAlready an obstacle - skip")
                 continue
+
+            # Generate the specific maze
+            maze = copy.deepcopy(ogMaze)
+            maze[i] = maze[i][0:j] + "#" + maze[i][j + 1:]
+
+            # Go thru the maze, exiting if outside area or if reach loop
+            row = guardRow
+            col = guardCol
+            visited = [(row, col, "u"),]
+            orientation = "u"
+            initCount = count
+            while True:
+                # Handle motion and rotation
+                if orientation == "u":
+                    if row == 0: # Out of bounds
+                        break
+                    row -= 1
+                    if maze[row][col] == "#":
+                        row += 1
+                        orientation = "r"
+                elif orientation == "r":
+                    if col == nCols - 1: # Out of bounds
+                        break
+                    col += 1
+                    if maze[row][col] == "#":
+                        col -= 1
+                        orientation = "d"
+                elif orientation == "d":
+                    if row == nRows - 1: # Out of bounds
+                        break
+                    row += 1
+                    if maze[row][col] == "#":
+                        row -= 1
+                        orientation = "l"
+                elif orientation == "l":
+                    if col == 0: # Out of bounds
+                        break
+                    col -= 1
+                    if maze[row][col] == "#":
+                        col += 1
+                        orientation = "u"
+                # check if visited 
+                if (row, col, orientation) in visited:
+                    count += 1
+                    break
+                else:
+                    visited.append((row, col, orientation))
+            if initCount == count:
+                print("\tOut of bounds")
+            else:
+                print("\tLoop!")
+    print(count)            
 
 if __name__ == "__main__":
     main()
