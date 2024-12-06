cd45bd7d25cc1530588340a7571d81a7ade60bfe
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-06 14:25:37 +1000

    FINISHED DAY 5 PART 2git status!

--- 05/sol_part2_attempt2.py
+++ 05/sol_part2_attempt2.py
@@ -31,6 +31,9 @@ def parse_orders():
     return orders
 
 def ordered(order, rules):
+    """
+    Returns list of orders
+    """
     i = 0
     nPages = len(order)
     while i < nPages:
@@ -41,14 +44,43 @@ def ordered(order, rules):
         i += 1
     return True
 
+def reduce_ruleset(order, rules):
+    ruleset = dict()
+    for page in order:
+        if page in rules:
+            ruleset[page] = list()
+            for prereq in rules[page]:
+                if prereq in order:
+                    ruleset[page].append(prereq)
+    return ruleset
+
+def parse_correct_order(order, rules):
+    # Establish each present pre-req for each page
+    newOrder = list()
+    while len(order) > 0:
+        rules = reduce_ruleset(order, rules)
+        minimumInDegree = 10000000
+        minimum = 1000000
+        for page in order:
+            inDegree = len(rules[page])
+            if inDegree < minimumInDegree:
+                minimumInDegree = inDegree
+                minimum = page
+        if minimumInDegree != 0:
+            raise ValueError("Minimum has a prerequisite!")
+        newOrder.append(minimum)
+        order.remove(minimum)
+    return newOrder 
+
 def main():
     rules = parse_rules()
     printOrders = parse_orders()
 
     result = 0
     for order in printOrders:
-        if ordered(order, rules):
-            result += order[(len(order) - 1) // 2]
+        if not ordered(order, rules):
+            correctOrder = parse_correct_order(order, rules)
+            result += correctOrder[(len(correctOrder) - 1) // 2]
 
     print(result)
 
