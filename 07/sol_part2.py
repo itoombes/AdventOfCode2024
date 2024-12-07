290064420edcf89715688df3ffa3a21516293e8e
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-07 16:31:42 +1000

    Now going thru all variants of operations

--- 07/sol_part2.py
+++ 07/sol_part2.py
@@ -1,6 +1,8 @@
 # itoombes, Advent of Code 2025
 # Day 7
 
+import copy
+
 def parse_input():
     if False:
         f = open("dummyInput.txt", "r")
@@ -16,17 +18,33 @@ def parse_input():
         equations.append((result, values))
     return equations 
 
+def generate_ops(nOps):
+    opSet = list()
+    ops = [0] * nOps
+    opSet.append(ops)
+    for i in range(0, nOps):
+        for ops in copy.deepcopy(opSet):
+            cop1 = copy.deepcopy(ops)
+            cop2 = copy.deepcopy(ops)
+            cop1[i] = 1
+            opSet.append(cop1)
+            cop2[i] = 2
+            opSet.append(cop2)
+    return opSet
+
+
 def is_possible(equation):
     print(f"Validate {equation}")
     # Iterate thru each possible operation comparison
-    ops = [0] * (len(equation[1]) - 1)
-    while True:
+    opSet = generate_ops(len(equation[1]) - 1)
+
+    for ops in opSet:
         currentTotal = equation[1][0]
         # Go through variant
         for i in range(1, len(equation[1])):
             if ops[i - 1] == 1:
                 currentTotal += equation[1][i]
-            else if ops[i - 1] == 2:
+            elif ops[i - 1] == 2:
                 currentTotal = int(str(currentTotal) + str(equation[1][i]))
             else:
                 currentTotal *= equation[1][i]
@@ -37,21 +55,7 @@ def is_possible(equation):
                 print(f"\tValid : {ops}")
                 return True
             print(f"\tInvalid : {currentTotal}")
-
-        # Iterate to next variant
-        i = 0
-        while True: 
-            if ops[i] == 0:
-                ops[i] = 1
-                for j in range(0, i):
-                    ops[j] = 0
-                break
-            i += 1
-            # If here, have no values that will match 
-            if i == len(ops):
-                print("\tNo valid values")
-                return False
-
+    return False
 
 def main():
     equations = parse_input()
