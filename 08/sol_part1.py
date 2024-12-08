619266f713ad314ab048674fc7d4d90e1638ad49
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-08 15:20:20 +1000

    Day 8 - now iterating over every valid combo

--- 08/sol_part1.py
+++ 08/sol_part1.py
@@ -19,10 +19,20 @@ def parse_map():
                     freqs[char] = [(r, c),]
     return (nRows, nCols, freqs)
 
+def locate_antinodes(nRows, nCols, antennae):
+    # Iterate from first to second last antenna
+    for i in range(0, len(antennae) - 1):
+        for j in range(i + 1, len(antennae)):
+            print(f"\t{antennae[i]}, {antennae[j]}")
+
 def main():
     nRows, nCols, freqs = parse_map()
     print(nRows, nCols, freqs)
-
+    locations = list()
+    # Get antinode locations
+    for f in freqs:
+        print(f)
+        candidates = locate_antinodes(nRows, nCols, freqs[f])
 
 if __name__ == "__main__":
     main()
