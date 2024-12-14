ad95ec9f60563c77d6e41fc277fe10e6604657a4
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-14 16:09:34 +1000

    Day 14 - finally have correct motion!

--- 14/sol_part1.py
+++ 14/sol_part1.py
@@ -2,6 +2,7 @@
 # Day 14
 
 DUMMY = "dummyinput.txt"
+EDUMMY = "dummydummy.txt"
 INPUT = "input.txt"
 
 WIDTH = 11
@@ -15,14 +16,15 @@ class Robot():
         self._px = int(px)
         self._py = int(py)
         self._vx = int(vx)
-        self._vy = int(vy)
+        self._vy = int(vy) # Moves towards top
 
-    def perform_iterations(self):
-        self._px = (self._px + (self._vx * SECONDS)) % WIDTH
-        self._py = (self._py + (self._vx * SECONDS)) % HEIGHT 
-    
-    def get_position(self):
+    def get_init_position(self):
         return (self._px, self._py)
+
+    def get_final_position(self):
+        x = (self._px + self._vx * SECONDS) % WIDTH
+        y = (self._py + self._vy * SECONDS) % HEIGHT
+        return (x, y)
 
     def __str__(self):
         return f"p={self._px},{self._py}, v={self._vx},{self._vy}"
@@ -33,16 +35,19 @@ def get_robots(file):
     for line in f.readlines():
         pos, vel = line.split(" ")
         pos = pos[2:].split(",")
-        vel = vel[2:-1].split(",")
+        vel = vel[2:].split(",")
         robots.append(Robot(pos[0], pos[1], vel[0], vel[1]))
     return robots
 
-def print_robots(robots):
+def print_robots(robots, final):
     environment = list()
     for i in range(0, HEIGHT):
         environment.append("." * WIDTH)
     for robot in robots:
-        x, y = robot.get_position()
+        if final:
+            x, y = robot.get_final_position()
+        else:
+            x, y = robot.get_init_position()
         if environment[y][x] == '.':
             environment[y] = environment[y][:x] + '1' + environment[y][x + 1:]
         else:
@@ -50,10 +55,15 @@ def print_robots(robots):
             environment[y] = environment[y][:x] + str(count) + environment[y][x + 1:]
     for line in environment:
         print(line)
+    print() # Newline
 
 def main():
     robots = get_robots(DUMMY)
-    print_robots(robots)
+    for robot in robots:
+        print(robot)
+        print(robot.get_final_position())
+    print_robots(robots, False)
+    print_robots(robots, True)
 
 
 if __name__ == "__main__":
