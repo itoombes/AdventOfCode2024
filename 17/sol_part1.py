7ea6d8266c605844476ee341cea952fe1024cb5b
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-17 15:16:20 +1000

    Day 17 - parsing combo, have adv instruction

--- 17/sol_part1.py
+++ 17/sol_part1.py
@@ -14,11 +14,39 @@ def read_from_file(file):
         opcodes.append(int(c))
     return a, opcodes
 
+class Machine():
+    def __init__(self, a_reg, opcodes):
+        self._A = a_reg
+        self._B = 0
+        self._C = 0
+        self._opcodes = opcodes
+        self._opcount = 0
+    
+    def next_instruction(self):
+        opcode = self._opcodes[self._opcount]
+        self._opcount += 1
+        # TODO : Parse the opcode
+        pass
+
+    def get_combo_value(self):
+        opcode = self._opcodes[self._opcount]
+        self._opcount += 1
+        if operand >= 0 and operand < 4:
+            return operand
+        if operand == 4:
+            return self._A
+        if operand == 5:
+            return self._B
+        if operand == 6:
+            return self._C
+        raise ValueError("Invalid combo value!")
+
+    def adv(self):
+        # Perform division
+        self._A = self._A // (2 ** self.get_combo_value()) 
 
 def main():
-    a, opcodes = read_from_file(INPUT)
-    print(a)
-    print(opcodes)
+    machine = Machine(read_from_file(DUMMY))
 
 if __name__ == "__main__":
     main()
