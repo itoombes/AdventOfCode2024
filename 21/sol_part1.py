bf095a9819795d8b4faa3c5f2d4c6ee3e1b4b945
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-21 15:43:16 +1000

    Day 21 - now storing robot chains as states, applying first bot direction buttons, need to add first buton

--- 21/sol_part1.py
+++ 21/sol_part1.py
@@ -26,22 +26,14 @@ def read_sequences():
     return sequences
 
 class BotChain():
-    def __init__(self):
-        # Bot 1 - user controlled, points to bot 2
-        self._bot1_r = self._bot1_c = None
-        # Bot 2 - controlled by bot 1, points to bot 3
-        self._bot2_r = self._bot2_c = None
-        # Bot 3 - controlled by bot 2, points to keypad
-        self._bot3_r = self._bot3_c = None
-        self._reset()
-
-    def reset(self):
-        self._bot1_r = 0
-        self._bot1_c = 2
-        self._bot2_r = 0
-        self._bot2_c = 2
-        self._bot3_r = 3
-        self._bot3_c = 2
+    def __init__(self, bot1_r, bot1_c, bot2_r, bot2_c, bot3_r, bot3_c, output):
+        self._bot1_r = bot1_r
+        self._bot1_c = bot1_c
+        self._bot2_r = bot2_r
+        self._bot2_c = bot2_c
+        self._bot3_r = bot3_r
+        self._bot3_c = bot3_c
+        self._output = output
     
     def check_panic(self):
         # Check row position
@@ -56,9 +48,38 @@ class BotChain():
                 return True
         return False
 
+    def get_successors(self):
+        
+        # Go thru each potential action sequence
+        successors = list()
+        # Apply motion
+        r = self._bot1_r
+        c = self._bot1_c
+        for row, col in ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)):
+            if row < 0 or row > 1:
+                continue
+            if col < 0 or col > 2:
+                continue
+            successors.append(BotChain(row, col, self._bot2_r, self._bot2_c, self._bot3_r, self._bot3_c, self._output))
+        return successors
+        # Apply button
+        bot2_response = ROBOT_PAD[self._bot1_r][self._bot1_c]
+
+    def __str__(self):
+        string = f"Bot 1 : ({self._bot1_r}, {self._bot1_c}) -> {ROBOT_PAD[self._bot1_r][self._bot1_c]}\n"
+        string += f"Bot 2 : ({self._bot2_r}, {self._bot2_c}) -> {ROBOT_PAD[self._bot2_r][self._bot2_c]}\n"
+        string += f"Bot 3 : ({self._bot3_r}, {self._bot3_c}) -> {KEYPAD[self._bot3_r][self._bot3_c]}\n"
+        string += f"Output : {self._output}\n"
+        return string
+
 def main():
     print(read_sequences())
-    control = BotChain()
+    control = BotChain(0, 2, 0, 2, 3, 2, list())
+    print(control)
+    for c in control.get_successors():
+        print("----------------")
+        print(c)
+        print("----------------")
 
 if __name__ == "__main__":
     main()
