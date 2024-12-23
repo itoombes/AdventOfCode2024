3f5a2856e46ff5ae146370f5aa09cf3aa11c3a1e
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-23 15:43:37 +1000

    Day 23 - was working on part 2 - messed up by not ensuring connectivity

---
+++ 23/sol_part2.py
@@ -0,0 +1,56 @@
+# itoombes, Advent of Code 2024
+# Day 23
+
+import copy
+
+DUMMY = 1
+if DUMMY:
+    INPUT = "dummy.txt"
+else:
+    INPUT = "input.txt"
+
+
+def read_graph():
+    f = open(INPUT, "r")
+    graph = dict()
+    for line in f.readlines():
+        line = line.replace("\n", "")
+        a, b = line.split("-")
+        if a not in graph.keys():
+            graph[a] = set()
+        if b not in graph.keys():
+            graph[b] = set()
+        graph[a].add(b)
+        graph[b].add(a)
+    return graph
+
+def build_connections(graph):
+    vertices = list(graph.keys())
+    print(vertices)
+    connections = list()
+    while len(vertices) > 0:
+        subgraph = set()
+        frontier = [vertices.pop(0),]
+        subgraph.add(frontier[0])
+        while len(frontier) > 0:
+            v = frontier.pop(0)
+            print(v)
+            for s in graph[v]:
+                print(f"\t{s}")
+                if s not in subgraph:
+                    subgraph.add(s)
+                    frontier.append(s)
+                    vertices.remove(s)
+        print(subgraph)
+        connections.append(subgraph)
+    return connections
+    
+
+
+
+def main():
+    graph = read_graph()
+    subgraphs = build_connections(graph)
+
+if __name__ == "__main__":
+    main()
