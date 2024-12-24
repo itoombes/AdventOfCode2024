b133afffacbe017afc07c516eae3080f789a9c10
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-24 23:48:52 +1000

    Day 24 - did some more work around family xmas. Cannot use permutation method - even generating the permuations takes an unreasonably large amount of time

--- 24/sol_part2.py
+++ 24/sol_part2.py
@@ -40,19 +40,23 @@ def parse_predicates():
         predicates.append((in1, in2, gate, out))
     return predicates
 
-def apply_predicates(values, predicates):
+def sort_predicates(values, predicates):
     variablePredicates = list()
     setCache = dict()
     keys = values.keys()
+    outputs = list()
     for i, p in enumerate(predicates):
-        in1, gate, in2, out = p
+        in1, in2, gate, out = p
+        # Collect outputs
+        outputs.append(out)
+        # Cache preprepared results
         if in1 in keys and in2 in keys:
             if gate == "AND":
                 setCache[i] = values[in1] & values[in2]
             elif gate == "OR":
-                setCache[i] == values[in1] | values[in2]
+                setCache[i] = values[in1] | values[in2]
             elif gate == "XOR":
-                setCache[i] == values[in1] ^ values[in2]
+                setCache[i] = values[in1] ^ values[in2]
         elif in1 in keys:
             if values[in1] == 1 and gate == "OR":
                 setCache[i] = 1
@@ -60,16 +64,13 @@ def apply_predicates(values, predicates):
                 setCache[i] = 0
         elif in2 in keys:
             if values[in2] == 1 and gate == "OR":
-                setPredicates.append(i)
                 setCache[i] = 1
             elif gate == "AND":
                 setCache[i] = 0
-        else:
+        if i not in setCache.keys():
             variablePredicates.append(i)
-    print(variablePredicates)
-    print(setCache)
+    return setCache, variablePredicates, outputs
     
-
 def extract_z_values(values):
     expected = parse_expected_out(values)
     nZ = len(bin(expected)) - 3 + 100 # Want two digit with leading zeros
@@ -78,12 +79,50 @@ def extract_z_values(values):
         values[zed] = b
     return values
 
+def confirm_valid(values, predicates, cache, outputs):
+    # Set the cached values
+    for i in cache.keys():
+        if outputs[i][0] == "z" and cache[i] != values[outputs[i]]:
+            return False
+        else:
+            values[outputs[i]] = cache[i]
+    # 
+
+
+def permutations(number):
+    # Generate every swap index
+    pairs = list()
+    for i in range(0, number - 1):
+        for j in range(i + 1, number):
+            pairs.append((i, j))
+
+    swaps = list()
+    for i in range(0, len(pairs) - 3):
+        print(i)
+        for j in range(i, len(pairs) - 2):
+            print(f"\t{j}")
+            for k in range(j, len(pairs) - 1):
+                print(f"\t{k}")
+                for l in range(k, len(pairs)):
+                    print(f"\t{l}")
+                    swaps.append((pairs[i][0], pairs[i][1],
+                                  pairs[j][0], pairs[j][1],
+                                  pairs[k][0], pairs[k][1],
+                                  pairs[l][0], pairs[l][1]))
+                    
+    print(pairs)
+
 def main():
+    # Extract data from files
     setValues = parse_init_values()
     setValues = extract_z_values(setValues)
-    print(setValues)
-    apply_predicates(setValues, parse_predicates())
-
+    predicates = parse_predicates()
+    cache, variablePredicates, outputs = sort_predicates(setValues, predicates)
+    # Generate permutations
+    for perm in permutations(len(predicates)):
+        values = copy.deepcopy(setValues)
+        variablePredicates = copy.deepcopy(variablePredicates)
+        print(perm)
 
 if __name__ == "__main__":
     main()
