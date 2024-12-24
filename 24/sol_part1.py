a347b206bf8011303e3a398b1ad9431d6ba37a15
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-24 15:24:39 +1000

    Day 24 - cmplete part 1!

--- 24/sol_part1.py
+++ 24/sol_part1.py
@@ -13,18 +13,46 @@ else:
     VALUES = "input1.txt"
     PREDICATES = "input2.txt"
 
-def parse_values():
+def parse_init_values():
     f = open(VALUES, "r")
     values = dict()
     for line in f.readlines():
-        values[line[0:3]] = line[-2:-1]
-    print(values)
+        values[line[0:3]] = int(line[-2:-1])
+    return values
 
 def parse_predicates():
-    pass
+    f = open(PREDICATES, "r")
+    predicates = list()
+    for line in f.readlines():
+        line = line.replace("\n", "")
+        in1, gate, in2, _, out = line.split(" ")
+        predicates.append((in1, in2, gate, out))
+    return predicates
+
+def apply_predicates(values, predicates):
+    while len(predicates) > 0:
+        in1, in2, gate, out = predicates.pop(0)
+        if in1 not in values.keys() or in2 not in values.keys():
+            predicates.append((in1, in2, gate, out))
+            continue
+        if gate == "AND":
+            values[out] = values[in1] & values[in2]
+        elif gate == "XOR":
+            values[out] = values[in1] ^ values[in2]
+        elif gate == "OR":
+            values[out] = values[in1] | values[in2]
+    return values
 
 def main():
-    parse_values()
+    values = parse_init_values()
+    predicates = parse_predicates()
+    values = apply_predicates(values, predicates)
+    out = 0
+    for k in values.keys():
+        if k[0] == "z":
+            print(f"{k}, {values[k]}")
+            out = out | (values[k] << int(k[1:]))
+    print(out)
 
 if __name__ == "__main__":
     main()
