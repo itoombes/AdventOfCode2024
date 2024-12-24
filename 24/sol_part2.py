b61dbbebe4759f8df8c0401f604f229ac0d2d7a7
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-25 09:33:17 +1000

    Day 24 - detecting predicates with incorrect Z values

--- 24/sol_part2.py
+++ 24/sol_part2.py
@@ -40,64 +40,62 @@ def parse_predicates():
         predicates.append((in1, in2, gate, out))
     return predicates
 
-def sort_predicates(values, predicates):
-    variablePredicates = list()
-    setCache = dict()
-    keys = values.keys()
-    outputs = list()
-    for i, p in enumerate(predicates):
-        in1, in2, gate, out = p
-        # Collect outputs
-        outputs.append(out)
-        # Cache preprepared results
-        if in1 in keys and in2 in keys:
-            if gate == "AND":
-                setCache[i] = values[in1] & values[in2]
-            elif gate == "OR":
-                setCache[i] = values[in1] | values[in2]
-            elif gate == "XOR":
-                setCache[i] = values[in1] ^ values[in2]
-        elif in1 in keys:
-            if values[in1] == 1 and gate == "OR":
-                setCache[i] = 1
-            elif gate == "AND":
-                setCache[i] = 0
-        elif in2 in keys:
-            if values[in2] == 1 and gate == "OR":
-                setCache[i] = 1
-            elif gate == "AND":
-                setCache[i] = 0
-        if i not in setCache.keys():
-            variablePredicates.append(i)
-    return setCache, variablePredicates, outputs
-    
 def extract_z_values(values):
+    zValues = dict()
     expected = parse_expected_out(values)
     nZ = len(bin(expected)) - 3 + 100 # Want two digit with leading zeros
     for i, b in enumerate(bin(expected)[2:]):
         zed = "z" + str(nZ - i)[1:]
-        values[zed] = b
-    return values
+        zValues[zed] = b
+    return zValues
 
-def check_valid(values, predicates):
-    return True 
+def check_valid(values, predicates, expected, zValues):
+    initValues = copy.deepcopy(values)
+    initPredicates = copy.deepcopy(predicates)
+    while len(predicates) > 0:
+        in1, in2, gate, out = predicates.pop(0)
+        if in1 not in values.keys() or in2 not in values.keys():
+            predicates.append((in1, in2, gate, out))
+            continue
+        if gate == "AND":
+            values[out] = values[in1] & values[in2]
+        elif gate == "XOR":
+            values[out] = values[in1] ^ values[in2]
+        elif gate == "OR":
+            values[out] = values[in1] | values[in2]
+    failZ = list()
+    out = 0
+    for k in values.keys():
+        if k[0] == "z":
+            out = out | (values[k] << int(k[1:]))
+    print(bin(out))
+    print(bin(expected))
+    for k in zValues.keys():
+        if k[0] == "z" and int(zValues[k]) != int(values[k]):
+            print(f"{values[k]}, {zValues[k]}")
+            failZ.append(k)
+    for p in initPredicates:
+        in1, in2, gate, out = p
+            
+
 
 def main():
     # Extract data from files
     setValues = parse_init_values()
-    setValues = extract_z_values(setValues)
+    zValues = extract_z_values(setValues)
+    expected = parse_expected_out(setValues) 
     setPredicates = parse_predicates()
     while True:
         values = copy.deepcopy(setValues)
         predicates = copy.deepcopy(setPredicates)
         print("Input wires to swap:")
         swapMem = list()
-        for i in range(1, 5):
+        for i in range(1, 1):
             while True:
                 try:
                     swap1, swap2 = input().split()
-                    swap1 = int(swap1)
-                    swap2 = int(swap2)
+                    swap1 = int(swap1) + 1 # Using LINE NUMBER, not index
+                    swap2 = int(swap2) + 1
                     in11, in12, gate1, out1 = predicates[swap1]
                     in21, in22, gate2, out2 = predicates[swap2]
                     predicates[swap1] = (in11, in12, gate1, out2)
@@ -111,7 +109,9 @@ def main():
         print("All swaps complete")
                 
 
-        if check_valid(values, predicates):
+        print(predicates)
+        input()
+        if check_valid(values, predicates, expected, zValues):
             print("Valid!")
             for swap in swapMem:
                 print(f"{swap[0]}, {swap[1]}")
