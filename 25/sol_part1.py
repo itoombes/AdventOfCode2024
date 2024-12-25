36b9f3539c68aaa8eb64e99bf01e5651ca8b6068
itoombes <124896022+itoombes@users.noreply.github.com>
2024-12-25 16:06:51 +1000

    xmas - complete part 1!

--- 25/sol_part1.py
+++ 25/sol_part1.py
@@ -3,15 +3,14 @@
 
 DUMMY = 0
 if DUMMY:
-    INPUT = "input.txt"
-else:
     INPUT = "dummy.txt"
+else:
+    INPUT = "input.txt"
 
 def parse_input():
     keys = list()
     tumblr = list()
     lines = open(INPUT, "r").readlines()
-    print(lines)
     i = 0
     while i < len(lines):
         line = lines[i].replace("\n", "")
@@ -23,25 +22,28 @@ def parse_input():
                 if lines[i + j][2] == "#": c += 1
                 if lines[i + j][3] == "#": d += 1
                 if lines[i + j][4] == "#": e += 1
-            print(f"K : {(a, b, c, d, e)}")
             keys.append((a, b, c, d, e))
         if line == ".....":
             for j in range(1, 6):
-                print(lines[i + j])
                 if lines[i + j][0] == "#": a += 1
                 if lines[i + j][1] == "#": b += 1
                 if lines[i + j][2] == "#": c += 1
                 if lines[i + j][3] == "#": d += 1
                 if lines[i + j][4] == "#": e += 1
-            print(f"T : {(a, b, c, d, e)}")
             tumblr.append((a, b, c, d, e))
         i += 8
-    print(tumblr)
-    print(keys)
     return keys, tumblr
 
 def main():
-    keys, keyholes = parse_input()
+    keys, tumblers = parse_input()
+    matchCount = 0
+    for p0, p1, p2, p3, p4 in tumblers:
+        for k0, k1, k2, k3, k4 in keys:
+            if p0 + k0 > 5 or p1 + k1 > 5 or p2 + k2 > 5 or p3 + k3 > 5 or p4 + k4 > 5:
+                continue
+            matchCount += 1
+    print(matchCount)
+
 
 if __name__ == "__main__":
     main()
